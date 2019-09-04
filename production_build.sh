489e531781f4cf4b83ccc1c02008b4c2ef92214f
Ben Abbatematteo <benabba95@gmail.com>
2019-09-04 17:45:47 -0400

    centering titles in blogpost and blog. justifying post content and excerpts. also printing some more in production_build to clarify why password is required.

--- production_build.sh
+++ production_build.sh
@@ -1,3 +1,6 @@
 JEKYLL_ENV=production jekyll build
+echo enter password to rsync
 rsync -r _site/* brown:/web/cs/web/sites/bigai/
+echo rsync complete
+echo enter password to chmod
 ssh -t brown "chmod 777 -R /web/cs/web/sites/bigai/*"
